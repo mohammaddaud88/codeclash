4b51a6211bc1bd3659883b4d00958c5a6c978d23
Mayank Srivastava <shreyansh2553@gmail.com>
2025-08-13 14:52:49 +0530

    Not working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      // This rule says: any request starting with "/api"
+      // should be sent to your backend server.
+      '/api': {
+        target: 'http://localhost:8000', // Your backend's address
+        // changeOrigin: true, // Recommended for virtual hosts
+      }
+    }
+  }
 })
